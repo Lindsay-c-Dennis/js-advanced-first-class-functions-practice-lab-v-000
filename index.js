c762c22624664901fcef2b08a14dd6118068448b
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-24 15:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
   return drivers.forEach(function (driver) {
-    console.log(driver);
+    console.log(driver.name);
   });
 }
