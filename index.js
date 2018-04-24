d1b816f3f5ec55534a0abd82524728eabc7518fb
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-24 17:00:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,7 @@ const driversByName = function (drivers) {
 const totalRevenue = function (drivers) {
   let totalRevenue = 0;
   
-  drivers.forEach()
+  drivers.forEach(function (driver) {
+    
+  })
 }
